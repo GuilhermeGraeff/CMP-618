64d04d43dd10940718ca726184d56363d5094e6d
Andrey <andrey.schoier@gmail.com>
2024-02-04 19:17:52 +0000

    atualizando train models

--- helper/train_models.py
+++ helper/train_models.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import pandas as pd
 import math
 import numpy as np
+import shap
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
@@ -30,6 +31,8 @@ def df_to_dataset(dataframe, shuffle=True):
 
     return dataset, labels
 
+def f(X):
+    return regression.predict([X[:, i] for i in range(X.shape[1])]).flatten()
 
 with open('../dados/data_links.txt') as file:
     next(file)
@@ -50,6 +53,8 @@ for type, data in datasets.items():
 
     train_dataframe, test_dataframe = train_test_split(dataframe, test_size=0.2)
     train_dataframe, val_dataframe = train_test_split(train_dataframe, test_size=0.2)
+
+    dataset = df_to_dataset(dataframe)
 
     train_dataset, train_labels = df_to_dataset(train_dataframe)
     test_dataset, test_labels = df_to_dataset(test_dataframe, False)
@@ -88,3 +93,15 @@ for type, data in datasets.items():
         file.write(f'{type}: loss = {loss}; accuracy = {accuracy}\n')
     
     network.save('../models/'+type+'.keras')
+
+    explainer = shap.KernelExplainer(f, val_dataset)
+
+    explainer = shap.DeepExplainer(model, background)
+
+    shap_values = e.shap_values(test_dataset)
+
+    shap_values = explainer.shap_values(train_dataset, nsamples=500)
+
+    plot = shap.force_plot(explainer.expected_value, shap_values, train_dataset, show=False) 
+
+    print(plot)
